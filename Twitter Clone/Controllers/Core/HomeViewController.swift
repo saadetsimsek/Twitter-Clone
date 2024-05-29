75b238954f7dc0e31cfce5f906509bd358a4dec5
saadetsimsek <saadetsimsek13@gmail.com>
2024-05-29 14:13:46 +0300

    add a custom cell to display Tweets inside the feed table view.

--- Twitter Clone/Controllers/Core/HomeViewController.swift
+++ Twitter Clone/Controllers/Core/HomeViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
+    
+    private let timeLineTableView : UITableView = {
+        let tableView = UITableView()
+        tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        view.addSubview(timeLineTableView)
+        
+        timeLineTableView.delegate = self
+        timeLineTableView.dataSource = self
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        timeLineTableView.frame = view.bounds
+    }
 
 }
 
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TweetTableViewCell.identifier, for: indexPath) as? TweetTableViewCell else{
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+}
